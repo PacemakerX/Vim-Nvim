1430975a244dd4a35d66bca9ded78db2c64500f7
Sparsh <sparsh.officialwork@gmail.com>
2025-07-06 13:20:23 +0530

    adds copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,16 @@
 return {
   "github/copilot.vim",
-  lazy = true,
+  event = "InsertEnter",  -- Lazy load only when you start typing
+  config = function()
+    -- Optional: tweak suggestion behavior
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+
+    -- Optional: custom keymap for accepting suggestion
+    vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', {
+      expr = true,
+      silent = true,
+      noremap = true
+    })
+  end,
 }
